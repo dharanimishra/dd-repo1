f0e17ddc1f84d289affde66d8e31a6d39de4f252
Dharani <dharanimishra@myziksana.com>
2013-10-12 10:12:48 +0530

    Added class level comments

--- myfirstdynamicproxy/src/main/java/com/poc/dynamicproxy/ProxyTest.java
+++ myfirstdynamicproxy/src/main/java/com/poc/dynamicproxy/ProxyTest.java
@@ -1,5 +1,8 @@
 package com.poc.dynamicproxy;
 
+/**
+ * This is the interface that will be dynamically implemented by the client class
+ **/
 public interface ProxyTest {
 
 	public void ping() throws ApplicationException;
